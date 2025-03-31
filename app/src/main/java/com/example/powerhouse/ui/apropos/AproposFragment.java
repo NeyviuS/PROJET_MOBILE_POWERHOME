51cec62ee4430c69b3fb5617571be894492f7cd7
NeyviuS <joscos2807@gmail.com>
2025-03-31 15:25:36 +0200

    ajout du drawer

---
+++ app/src/main/java/com/example/powerhouse/ui/apropos/AproposFragment.java
@@ -0,0 +1,37 @@
+package com.example.powerhouse.ui.apropos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.powerhouse.databinding.FragmentAproposBinding;
+
+public class AproposFragment extends Fragment {
+
+    private FragmentAproposBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SlideshowViewModel slideshowViewModel =
+                new ViewModelProvider(this).get(SlideshowViewModel.class);
+
+        binding = FragmentAproposBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSlideshow;
+        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
